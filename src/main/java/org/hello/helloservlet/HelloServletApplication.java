ca3e6e89b6a6f3a709f309efd64b51a97e73aeb5
thdwj <thdwjdgkr123@naver.com>
2025-07-02 21:16:00 +0900

    Initial commit

---
+++ src/main/java/org/hello/helloservlet/HelloServletApplication.java
@@ -0,0 +1,13 @@
+package org.hello.helloservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hello/helloservlet/HelloServletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hello.helloservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
